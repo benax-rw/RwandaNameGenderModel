f0cca22d499eda100714c55765673222671f2981
Gabriel Baziramwabo <gbaziramwabo@benax.rw>
2025-05-01 11:50:21 +0200

    inference updated

--- inference.py
+++ inference.py
@@ -1,30 +1,24 @@
 import joblib
 
 # Load the trained model and vectorizer
-model_path = "model/logistic_model.joblib"
-vectorizer_path = "model/vectorizer.joblib"
-
-model = joblib.load(model_path)
-vectorizer = joblib.load(vectorizer_path)
+model = joblib.load("model/logistic_model.joblib")
+vectorizer = joblib.load("model/vectorizer.joblib")
 
 # Function to make predictions on user input
 def predict_gender(name):
-    # Convert the name to lowercase to match training preprocessing
-    name = name.lower()
-    
-    # Transform the name using the loaded vectorizer
+    name = name.strip().lower()
+    if not name:
+        return "Invalid input"
     name_vector = vectorizer.transform([name])
-    
-    # Predict the gender
     gender_pred = model.predict(name_vector)
-    
-    return gender_pred[0]
+    return gender_pred[0].capitalize()
 
 # Interactive inference
+print("RwandaNameGenderModel - Gender Prediction from Rwandan Name(s)")
 while True:
     user_input = input("Enter a name to predict gender (or type 'exit' to stop): ")
-    if user_input.lower() == 'exit':
+    if user_input.lower().strip() == 'exit':
         print("Exiting...")
         break
     gender = predict_gender(user_input)
-    print(f"The predicted gender for '{user_input}' is: {gender.capitalize()}")
+    print(f"The predicted gender for '{user_input}' is: {gender}\n")
